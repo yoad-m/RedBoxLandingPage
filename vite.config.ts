a0c92475e7115175623e5b4a00708e40d064b420
Yoad Mousai <yoadmousai@Yoads-MacBook-Pro.local>
2025-11-17 22:31:09 +0200

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+      base: "/RedBoxLandingPage/",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
